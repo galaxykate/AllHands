bbbcebf8998a7e7317c77084ea3745a109033b43
Kate Compton <galaxykate@gmail.com>
2022-09-02 10:11:34 +0300

    Adding preliminary hand data UI, app has new 'hands' property, new 'handtest' app

--- web/js/sketches/sketch-handtest.js
+++ web/js/sketches/sketch-handtest.js
@@ -32,18 +32,43 @@ sketches["handtest"] = {
 	// If you want Vue to see thing
 	points: [],
 	init(p) {
+		p.fill(0, 0, 100, 1)
 		console.log("INIT HAND TEST", this.id)
 
 	},
 
 	draw(p, t, dt) {
-		this.frame += 1
 		// p.background(0)
-		p.fill(0, 0, 0, .02)
+		p.fill(0, 0, 100, 1)
 		p.rect(0, 0, p.width, p.height)
 
+		p.push()
+		p.translate(p.width/2, p.height/2)
+		app.hands.forEachHand((hand, handIndex) => {
+			p.fill(handIndex*100, 100, 50)
+			hand.data.forEach(v => {
+				// console.log(v)
+				p.circle(...v, 5)
+			})
 
-		
+			hand.data.slice(0,5).forEach(v => {
+				p.noFill()
+				p.circle(...v, 15)
+			})
+		})
+
+		p.pop()
+
+
+
+		// app.hands.forEachHand((hand, handIndex) => {
+		// 	hand.forEachFinger((finger, fingerIndex) => {
+		// 		p.fill(fingerIndex*10, )
+		// 		finger.joints.forEach((joint, jointIndex) => {
+		// 			p.circle(...j, 50 - jointIndex*5)
+		// 		})
+		// 	}) 
+		// })
 	}
 
 }
