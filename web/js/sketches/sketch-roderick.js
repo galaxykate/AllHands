7134f4760ce56e9ee092c1035d64dd0beb47d820
roderickseow <46728592+roderickseow@users.noreply.github.com>
2022-09-02 14:40:54 +0300

    moving platforms!

--- web/js/sketches/sketch-roderick.js
+++ web/js/sketches/sketch-roderick.js
@@ -1,43 +1,109 @@
-Vue.component("controls-roderick", {
-	template: `<div>MY TEST CONTROLS HERE</div>`,
-	props: ["app"]
+
+Vue.component("controls-handtest", {
+	template: `<div>
+
+	</div>`,
+	methods: {
+		togglePause() {
+
+		},
+		chaos() {
+			console.log("CHAOS")
+			this.sketch.forceMultiplier *= 1.2
+		}
+	},
+	props: ["app", "sketch"]
 })
 
-Vue.component("debug-roderick", {
-	template: `<div>MY TEST DEBUG HERE!!!!!</div>`,
-	props: ["app"]
+Vue.component("debug-handtest", {
+	template: `<div>
+
+	</div>`,
+	props: ["app", "sketch"]
 })
+
 
 sketches["roderick"] = {
 	id: "roderick",
-	desc: "Small platformer",
-	gameObjects: [],
-	init(p) {
-		console.log("INIT SKETCH", this.id)
-		this.world = planck.World(planck.Vec2(0, -10))
-		player = new PlayerController(50, 50, this.world)
-		this.gameObjects.push(player);
-	},
-	draw(p, t, dt) {
-		p.background(100)
-		// Update
-		for(var i = 0; i < this.gameObjects.length; i++)
-		{
-			this.gameObjects[i].update(dt)
-			//this.gameObjects[i].x += dt
-		}
-		this.world.step(1/60);
+	frame: 0,
+	forceMultiplier: 1,
+	desc: "Example things!",
 
-		// Draw
-		p.background(0)
-		for (var i = 0; i < this.gameObjects.length; i++) {
-			p.strokeWeight(1)
-			p.stroke(100)
-			// p.noStroke()
-			// Set the stroke color to be LIGHTER
-			go = this.gameObjects[i];
-			p.rect(go.x, go.y, 10, 10)
-		}
+	// If you want Vue to see thing
+	points: [],
+	init(p) {
+		p.fill(0, 0, 100, 1)
+		console.log("INIT HAND TEST", this.id)
+
+	},
+
+	draw(p, t, dt) {
+		// p.background(0)
+		p.fill(0, 0, 100, 1)
+		p.rect(0, 0, p.width, p.height)
+
+		p.push()
+		p.translate(p.width/2, p.height/2)
+		app.hands.forEachHand((hand, handIndex) => {
+			p.noStroke()
+			p.fill(handIndex*100, 100, 50)
+			hand.data.forEach((v,vIndex) => {
+				// console.log(v)
+				p.circle(...v, 5)
+				p.fill(0)
+				p.text(vIndex, ...v)
+			})
+
+			hand.data.slice(0,5).forEach(v => {
+				p.noFill()
+				p.circle(...v, 15)
+
+				p.fill(0)
+
+			})
+		})
+
+		
+
+
+
+		app.hands.forEachHand((hand, handIndex) => {
+			// Draw as circles
+			hand.forEachFinger((finger, fingerIndex) => {
+				p.noStroke()
+				p.fill(fingerIndex*20 + 150*handIndex, 100, 50, .4)
+				finger.joints.forEach((joint, jointIndex) => {
+					joint.draw(p, 10)
+
+				})
+			}) 
+
+			// // Draw as tube
+			hand.forEachFinger((finger, fingerIndex) => {
+				p.strokeWeight(10)
+				p.noFill()
+				p.stroke(fingerIndex*20 + 150*handIndex, 100, 50, 1)
+				p.beginShape()
+				finger.joints.forEach((joint, jointIndex) => {
+					joint.vertex(p)
+					// joint.draw(p, 10)
+				})
+				p.endShape()
+			}) 
+			p.noStroke()
+				
+			// Just fingertips
+			hand.forEachFinger((finger, fingerIndex) => {
+				p.noFill()
+				p.strokeWeight(1)
+				p.stroke(fingerIndex*20 + 150*handIndex, 100, 50, .4)
+				finger.fingerTip.draw(p, 50)
+				
+				//console.log([finger.fingerTip.x, finger.fingerTip.y])
+			}) 
+		})
+
+		// p.pop()
 	}
 
 }
--- web/js/sketches/sketch-thomas.js
+++ web/js/sketches/sketch-thomas.js
@@ -12,6 +12,7 @@ sketches["thomas"] = {
 	id: "thomas",
 	desc: "Small platformer",
 	gameObjects: [],
+	floor: {},
 	init(p) {
 		console.log("INIT SKETCH", this.id)
 		this.world = planck.World(planck.Vec2(0, 10))
@@ -19,8 +20,8 @@ sketches["thomas"] = {
 		this.gameObjects.push(player);
 		//var platform = new Platform(1, 20, 23, 1, this.world)
 		//this.gameObjects.push(platform);
-		var floor = new FloorController(1, 24, this.world);
-		this.gameObjects.push(floor)
+		this.floor = new FloorController(1, 24, this.world);
+		this.gameObjects.push(this.floor)
 		// Level bounds
 		this.gameObjects.push(new Platform(0, 0, 1, 25, this.world))
 		this.gameObjects.push(new Platform(24, 0, 1, 25, this.world))
@@ -28,6 +29,18 @@ sketches["thomas"] = {
 	},
 	draw(p, t, dt) {
 		// Update
+
+		var index = 0
+		app.hands.forEachHand((hand, handIndex) => {
+			hand.forEachFinger((finger, fingerIndex) => {
+				this.floor.points[index].y =  25*(finger.fingerTip.y / p.height) + 20
+				index++
+			})
+		})
+
+		// for(var i = 0; i < this.floor.points.length; i++)
+		// 	this.floor.points[i].y = finger.fingerTip[i].y
+
 		for(var i = 0; i < this.gameObjects.length; i++)
 			this.gameObjects[i].update(p, dt)
 		this.world.step(dt);
