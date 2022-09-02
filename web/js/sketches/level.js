ae0a580f41bbd170bb0d223756b38b9a48387287
SuenjeAlice <36443029+SuenjeAlice@users.noreply.github.com>
2022-09-02 14:47:05 +0300

    added level create

--- web/js/sketches/level.js
+++ web/js/sketches/level.js
@@ -4,7 +4,12 @@ function obsticale(initX, initY, width, height, world)
 	return obsticale
 }
 
-function createLevel()
+function createLevel(world)
 {
-
+    var gameObjectsLocal = []
+    gameObjectsLocal.push(obsticale(5, 15, 3, 1, world));
+    gameObjectsLocal.push(obsticale(10, 12, 3, 1, world));
+    gameObjectsLocal.push(obsticale(15, 8, 3, 1, world));
+    gameObjectsLocal.push(obsticale(20, 3, 3, 1, world));
+    return gameObjectsLocal
 }
