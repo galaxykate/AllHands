8fc06fb0edcb009e63de33861c84569be61a0101
YashBhartia00 <51514725+YashBhartia00@users.noreply.github.com>
2022-09-02 12:31:08 +0300

    finger music

--- web/js/sketches/sketch-yash.js
+++ web/js/sketches/sketch-yash.js
@@ -24,15 +24,15 @@ Vue.component("controls-yash", {
 			context.resume()
 
 
-			var intervalId = window.setInterval(this.c_music, 1000);
+			var intervalId = window.setInterval(this.c_music, this.sketch.freq*1000);
 			
 		
 					},
 		c_music(){
 			const synth = new Tone.AMSynth().toDestination()
 
-			const note = Tone.Time(1).toNotation() 
-			const tune = this.sketch.soundNum
+			const note = Tone.Time(this.sketch.freq).toNotation() 
+			const tune = this.sketch.sounds[0]
 
 			synth.triggerAttackRelease(Tone.Midi(tune).toMidi(), note)
 
@@ -48,6 +48,11 @@ Vue.component("debug-yash", {
 		<h3>ID: {{sketch.id}}</h3>
 		{{sketch.frame}}
 		<div>sn: {{sketch.soundNum}}</div>
+		<div>
+			<div  v-for="pt in sketch.sounds">
+				{{pt.toFixed(2)}}
+			</div>
+		</div>
 	</div>`,
 	props: ["app","sketch"]
 })
@@ -57,39 +62,40 @@ sketches["yash"] = {
 	id: "yash",
 	desc: "Example things!",
 	soundNum: 40,
+	freq: 0.4,
+	sounds: [],
 	init(p) {
 		console.log("INIT SKETCH", this.id)
-
+		for(var i=0; i<10; i++){
+			this.sounds.push(20*i)
+		}
 	},
 	draw(p, t, dt) {
-
+		this.frame += 1
 		// p.background(0)
 		p.fill(0, 0, 0, 1)
 		p.rect(0, 0, p.width, p.height)
-			
-		let count = 100
-		let size = 10
-		for (var i = 0; i < count; i++) {
-			size = 16 + 10*Math.sin(i)
-			let x = i*p.width/count
-			// let y = i*10 + 120*Math.sin(t + i)
 
-			let y = 400*(noise(i*.02, t*.2) - .5) + 300
+		p.push()
+		p.translate(p.width/2, p.height/2)
 
-			let hue = (100*noise(i , t))%360
-			// Set the inner color to be DARKER
-			// let sat = 50 + 50*Math.sin(i +t )
-			let sat = 100
-			p.fill(hue, sat, 30, .2)
 
-			p.strokeWeight(2)
-			p.stroke(hue + 20, sat, 50)
-			// p.noStroke()
-			// Set the stroke color to be LIGHTER
-			// p.arc(x, y, size, size)
-			p.arc(x, y, size, size, Math.sin(t+i)*3.14, Math.sin(t)*3.14)
-		}
-		
+		app.hands.forEachHand((hand, handIndex) => {
+				hand.forEachFinger((finger, fingerIndex) => {
+
+				var samp = finger.fingerTip.v[1]
+				samp=(samp+(p.height/2))*127/p.height
+					
+				this.sounds[fingerIndex+handIndex*5] = samp
+				
+				p.noFill()
+				p.strokeWeight(1)
+				p.stroke(fingerIndex*20 + 150*handIndex, 100, 50, .8)
+				finger.fingerTip.draw(p, 5)
+			}) 	
+				
+		})
+
+		p.pop()
 	}
-
 }
